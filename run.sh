b631032f3adce05c7ffca8716985ecabf8b6a5d9
Frank <frank@void0.ch>
2022-05-22 22:58:19 +0200

    fix

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 
 # run this: git reset --hard && git pull && chmod +x run.sh && ./run.sh
 
+ARTIFACT_ID=$(date +%Y-%m-%dT%H-%M)_$(git rev-parse --short HEAD)
+
+
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
-docker run -d -p 40001:80 ghcr.io/frnkst/honeypot-frontend:2022-05-22t20-51_8e95825
-docker run -d -p 40002:3000 ghcr.io/frnkst/honeypot-backend:2022-05-22t20-49_8e95825
+docker run -d -p 40001:80 ghcr.io/frnkst/honeypot-frontend:$ARTIFACT_ID
+docker run -d -p 40002:3000 ghcr.io/frnkst/honeypot-backend:$ARTIFACT_ID
 docker run -d -p 40003:5432 -e POSTGRES_PASSWORD=45432dfdf*dfdfl -e POSTGRES_USER=honey postgres
 rm server.key
 ssh-keygen -q -N '' -f server.key
