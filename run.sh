d809bbbb7f6e673bf054c26b583904a2746f5750
Frank <frank@void0.ch>
2022-05-22 22:33:29 +0200

    maybe now

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-VERSION=2022-05-22t20-18_7a69391
+VERSION=2022-05-22t20-28_bfee435
 
 docker rm $(docker ps -a -q)
 docker run -d -p 5555:80 ghcr.io/frnkst/honeypot-frontend:$VERSION
 docker run -d -p 3000:3000 ghcr.io/frnkst/honeypot-backend:$VERSION
-docker run --name honeypot-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
-ssh-keygen -f server.key
-python3 honeypot/ssh-honeypot.py
+docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=45432dfdf*dfdfl -e POSTGRES_USER=honey postgres
+rm server.key
+ssh-keygen -q -N '' -f server.key
+nohub python3 honeypot/ssh-honeypot.py > honeypot-log.txt &
