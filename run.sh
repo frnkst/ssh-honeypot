ba52c150052c58e08d2c937cff69124ddd4db225
Frank <frank@void0.ch>
2022-05-22 22:52:14 +0200

    more

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 
 docker kill $(docker ps -q)
 docker rm $(docker ps -a -q)
-docker run -d -p 5555:80 ghcr.io/frnkst/honeypot-frontend:2022-05-22t20-49_8e95825
+docker run -d -p 5555:80 ghcr.io/frnkst/honeypot-frontend:2022-05-22t20-51_8e95825
 docker run -d -p 6666:3000 ghcr.io/frnkst/honeypot-backend:2022-05-22t20-49_8e95825
 docker run -d -p 5432:5432 -e POSTGRES_PASSWORD=45432dfdf*dfdfl -e POSTGRES_USER=honey postgres
 rm server.key
