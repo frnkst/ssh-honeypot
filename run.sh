bfee435db03b7db6c07b5e32e047c5990c464f3d
Frank <frank@void0.ch>
2022-05-22 22:28:30 +0200

    fix?

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 VERSION=2022-05-22t20-18_7a69391
 
-docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
 docker run -d -p 5555:80 ghcr.io/frnkst/honeypot-frontend:$VERSION
 docker run -d -p 3000:3000 ghcr.io/frnkst/honeypot-backend:$VERSION
 docker run --name honeypot-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
