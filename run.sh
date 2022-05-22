669187cc0a18fc1a4edb4bd2fbb37ab40e92c942
Frank <frank@void0.ch>
2022-05-22 22:20:36 +0200

    let's see

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-docker run -d -p 8080:80 ghcr.io/frnkst/honeypot-frontend:2022-05-22t19-58_39cee2
-docker run -d -p 8080:80 ghcr.io/frnkst/honeypot-backend:2022-05-22t19-58_39cee2
+VERSION=2022-05-22t20-18_7a69391
+
+docker run -d -p 8080:80 ghcr.io/frnkst/honeypot-frontend:$VERSION
+docker run -d -p 8080:80 ghcr.io/frnkst/honeypot-backend:$VERSION
 docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
 python3 ssh-honeypot.py
