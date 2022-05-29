8d4744c99e830c6cbbd686b8b1213ae517195efe
Frank <frank@void0.ch>
2022-05-29 08:30:14 +0200

    add city country isp

--- honeypot/ssh-honeypot.py
+++ honeypot/ssh-honeypot.py
@@ -11,7 +11,11 @@ SSH_PORT = 2222
 LOGFILE = 'logins.txt'
 LOGFILE_LOCK = threading.Lock()
 
-db = SimpleConnectionPool(1, 10,host='127.0.0.1',database='honeypotdb',user='honeypot_user',password='weeoiio459drv',port=5432)
+db = SimpleConnectionPool(1, 10, host='127.0.0.1', database='honeypotdb',
+                          user='honeypot_user', password='weeoiio459drv',
+                          port=5432)
+iplist = {}
+
 
 @contextmanager
 def get_connection():
@@ -21,25 +25,31 @@ def get_connection():
     finally:
         db.putconn(con)
 
-def get_ip_info(ip):
-    response = requests.get("http://ip-api.com/json/" + ip)
-    response.raise_for_status()
 
-    data = response.json()
-    print(data)
-    print(data['city'])
-    print(data['isp'])
+def get_ip_info(ip):
+    if ip not in iplist:
+        response = requests.get("http://ip-api.com/json/" + ip)
+        response.raise_for_status()
+        data = response.json()
+
+        iplist[ip] = {
+                'city': data['city'],
+                'country': data['country'],
+                'isp': data['isp']
+            }
+
+    return iplist[ip]
 
 
 def write_to_db(ip, username, password):
-    get_ip_info(ip)
+    ip_info = get_ip_info(ip)
 
     with get_connection() as conn:
         try:
             cursor = conn.cursor()
 
-            postgres_insert_query = """INSERT INTO logins (timestamp , ip, username, password) VALUES (%s,%s,%s,%s)"""
-            record_to_insert = (datetime.now(), ip, username, password)
+            postgres_insert_query = """INSERT INTO logins (timestamp , ip, username, password, city, country, isp) VALUES (%s,%s,%s,%s,%s,%s,%s)"""
+            record_to_insert = (datetime.now(), ip, username, password, ip_info['city'], ip_info['country'], ip_info['isp'])
             cursor.execute(postgres_insert_query, record_to_insert)
             cursor.close()
             conn.commit()
@@ -47,7 +57,8 @@ def write_to_db(ip, username, password):
             print("someting went wrong", e)
             conn.rollback()
 
-class SSHServerHandler (paramiko.ServerInterface):
+
+class SSHServerHandler(paramiko.ServerInterface):
     def __init__(self, client_ip):
         self.client_ip = client_ip
         self.event = threading.Event()
@@ -56,9 +67,9 @@ class SSHServerHandler (paramiko.ServerInterface):
         write_to_db(self.client_ip, username, password)
         return paramiko.AUTH_FAILED
 
-
     def get_allowed_auths(self, username):
         return 'password'
+
 
 def handleConnection(client, addr):
     client_ip = addr[0]
@@ -81,12 +92,13 @@ def main():
         server_socket.bind(('', SSH_PORT))
         server_socket.listen(100)
 
-        paramiko.util.log_to_file ('paramiko.log')
+        paramiko.util.log_to_file('paramiko.log')
 
-        while(True):
+        while (True):
             try:
                 client_socket, client_addr = server_socket.accept()
-                threading.Thread.start(handleConnection(client_socket, client_addr))
+                threading.Thread.start(
+                    handleConnection(client_socket, client_addr))
             except Exception as e:
                 print("ERROR: Client handling")
                 print(e)
@@ -95,5 +107,6 @@ def main():
         print("ERROR: Failed to create socket")
         print(e)
         sys.exit(1)
+
 
 main()
