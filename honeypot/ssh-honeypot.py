7a5f87bb747e01d121fef1f92cb115a1dffef2ce
Frank <frank@void0.ch>
2022-11-26 21:22:13 +0100

    change host

--- honeypot/ssh-honeypot.py
+++ honeypot/ssh-honeypot.py
@@ -16,8 +16,9 @@ DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_PORT = os.getenv('DB_PORT')
+DB_HOST = os.getenv('DB_HOST')
 
-db = SimpleConnectionPool(1, 10, host='127.0.0.1', database=DB_NAME,
+db = SimpleConnectionPool(1, 10, host=DB_HOST, database=DB_NAME,
                           user=DB_USER, password=DB_PASSWORD,
                           port=DB_PORT)
 iplist = {}
