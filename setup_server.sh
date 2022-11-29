3caa216d87ff09efe49978d19f321f5031a324d6
Frank <frank@void0.ch>
2022-11-29 21:31:29 +0100

    c

--- setup_server.sh
+++ setup_server.sh
@@ -10,11 +10,11 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get updat
+apt-get update
 apt-get install -y docker-ce docker-ce-cli docker-compose containerd.io docker-compose-plugin
 
 # redirect port
 /sbin/iptables -A PREROUTING -t nat -p tcp --dport 22 -j REDIRECT --to-port 2222
 
-curl -O https://raw.githubusercontent.com/frnkst/ssh-honeypot/main/docker-compose.yaml
+curl -O -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/frnkst/ssh-honeypot/main/docker-compose.yaml
 docker-compose down --volumes  && docker-compose up -d
