c763092ddc6c7421a4db0785649d37f75933da1c
Frank <frank@void0.ch>
2022-11-29 21:27:52 +0100

    a

--- setup_server.sh
+++ setup_server.sh
@@ -11,7 +11,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get updat
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli docker-compose containerd.io docker-compose-plugin
 
 # redirect port
 /sbin/iptables -A PREROUTING -t nat -p tcp --dport 22 -j REDIRECT --to-port 2222
